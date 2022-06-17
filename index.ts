669c2a6be4b07c5113b8a4f436dd8b0add568d8c
emih <emi.programming@gmail.com>
2022-06-17 14:20:05 +0900

    Learn advancced generics

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-const foo = <T extends string | number>(arg: T) => {
-    if (typeof arg === "string") {
-        return { value: arg.toUpperCase() };
-    }
-    return { value: arg.toFixed() };
+const getPriority = <T, K extends keyof T>(obj: T, key: K) => {
+    return obj[key];
 };
+const setPriority = <T, K extends keyof T>(obj: T, key: K, value: T[K]) => {
+    obj[key] = value;
+};
+
+const obj = {
+    foo: 1,
+    bar: 2,
+    baz: 3,
+};
+
+// const hoge = getPriority(obj, "baz");
+// baz:number
+
+setPriority(obj, "bar", 100);
