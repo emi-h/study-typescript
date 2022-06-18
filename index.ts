2a92cd34e9204bcaf06b57ddfdd527299263fde3
emih <emi.programming@gmail.com>
2022-06-18 13:27:05 +0900

    Learn declare

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
-import { PartialDeep } from "type-fest";
-type User = {
-    name: string;
-    age: number | null;
-    address: {
-        country: "US" | "UK" | "JP";
-    }
-};
-
-type PartialUser = PartialDeep<User>;
-
-const user: PartialUser = {
-    name: "emi",
-    address: {}
-};
+process.env.FOO
