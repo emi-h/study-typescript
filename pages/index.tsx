3267716e6e85f6bcb3fa6048b619f1614a1c04f6
emih <emi.programming@gmail.com>
2022-06-18 15:40:01 +0900

    Add input button function and input text function

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,8 @@ import type { NextPage } from 'next'
 import { useState } from 'react';
 
 const Home: NextPage<{ foo: number }> = (props) => {
-  const [todos, setTodos] = useState([
-    { id: Math.random(), label: "TODO1", isDone: true },
-    { id: Math.random(), label: "TODO2", isDone: true },
-    { id: Math.random(), label: "TODO3", isDone: true },
-    { id: Math.random(), label: "TODO4", isDone: false },
-  ]);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const toggle = (e) => {
     setTodos((prevTodos) => {
@@ -18,14 +14,28 @@ const Home: NextPage<{ foo: number }> = (props) => {
         return todo;
       })
     });
-  }
+  };
+
+  const input = (e) => {
+    setText(e.target.value);
+  };
+
+  const add = () => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { id: Math.random(), label: text, isDone: false }
+      ]
+    });
+    setText("");
+  };
 
   return (
     <div className='w-96 mx-auto p-20'>
       <h1 className='text-xl font-bold'>Todo</h1>
       <div className='flex gap-x-2'>
-        <input type="text" className='border border-black' />
-        <button className='border border-black shrink-0 px-2'>追加</button>
+        <input type="text" value={text} onChange={input} className='border border-black' />
+        <button onClick={add} className='border border-black shrink-0 px-2'>追加</button>
       </div>
       <ul className='mt-4 y-2'>
         {todos.map((todo) => (
